55e766ea57582dedadfbc8df8375a508df2bbc3d
AlenaDmit <dmitalyona@yandex.ru>
2016-12-01 22:46:18 +0300

    useless tasks deleted

--- gulp/paths/tasks.js
+++ gulp/paths/tasks.js
@@ -9,10 +9,6 @@ module.exports = [
   './gulp/tasks/js.foundation.js',
   './gulp/tasks/css.foundation.js',
   './gulp/tasks/js.process.js',
-  './gulp/tasks/js.process1.js',
-  './gulp/tasks/js.process2.js',
-  './gulp/tasks/js.preload.js',
-  './gulp/tasks/js.blog.js',
   './gulp/tasks/js.lint.js',
   './gulp/tasks/copy.image.js',
   './gulp/tasks/copy.fonts.js'
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,8 +3,6 @@
 module.exports = function() {
   $.gulp.task('watch', function() {
     $.gulp.watch('./source/js/**/*.js', $.gulp.series('js.process'));
-    $.gulp.watch('./source/js/**/*.js', $.gulp.series('js.blog'));
-    $.gulp.watch('./source/js/**/*.js', $.gulp.series('js.preload'));
     $.gulp.watch('./source/style/**/*.scss', $.gulp.series('sass'));
     $.gulp.watch('./source/template/**/*.jade', $.gulp.series('jade'));
     $.gulp.watch('./source/images/**/*.*', $.gulp.series('copy.image'));
